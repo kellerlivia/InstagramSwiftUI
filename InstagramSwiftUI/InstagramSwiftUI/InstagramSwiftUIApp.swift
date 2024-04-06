202011c024796dda907610dd234400db5ecbb7e1
kellerlivia <rm94275@fiap.com.br>
2024-04-06 10:51:22 -0300

    feat: starting project

---
+++ InstagramSwiftUI/InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Livia Keller on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ InstagramSwiftUI/InstagramSwiftUI/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  InstagramSwiftUI
+//
+//  Created by Livia Keller on 06/04/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
